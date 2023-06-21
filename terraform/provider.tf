dc6025dacaf54a2c83c5ba148799bc7a5958f479
rathoreprerna <115581540+rathoreprerna@users.noreply.github.com>
2023-06-21 10:22:04 +0100

    Add files via upload

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,3 +11,20 @@ resource "random_string" "flask-secret-key" {
   special          = true
   override_special = "/@\" "
 }
+
+# Declare the Docker provider
+terraform {
+  required_providers {
+    # Set the required provider and versions
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.22.0"
+    }
+  }
+}
+
+# Add the Docker provider configurations
+provider "docker" {
+  # Configuration options
+  host = "npipe:////.//pipe//docker_engine"
+}
