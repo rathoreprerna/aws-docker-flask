434a807cdfa09213b4f44d21883ddd5da76ec975
rathoreprerna <115581540+rathoreprerna@users.noreply.github.com>
2023-06-21 11:32:28 +0100

    Delete docker_image.tf

--- terraform/docker_image.tf
+++
@@ -1,8 +0,0 @@
-# Create a docker image resource
-resource "docker_image" "flask_image" {               
-  name = "flask_image"
-  build {  
-    path = "../aws-docker-flask"
-    dockerfile = "dockerfile"
-  }
-}
